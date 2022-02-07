2c974b7c5a0b6cc6cdd568606d57600b490a3625
jhnnsrs <jhnnsrs@gmail.com>
2022-02-07 19:44:27 +0100

    test for field aliasing

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -5,6 +5,7 @@ from turms.compat.funcs import unparse
 from graphql.language import parse
 from turms.plugins.enums import EnumsPlugin
 from turms.plugins.inputs import InputsPlugin
+from turms.stylers.snake import SnakeNodeName
 
 
 def test_config():
@@ -84,3 +85,41 @@ def test_small():
     generated = unparse(ast.fix_missing_locations(md))
     assert "from enum import Enum" in generated, "EnumPlugin not working"
     assert "class HelloWorldOrder(str, Enum):" in generated, "EnumPlugin not working"
+
+
+def test_snake_case_styler():
+    schema = parse(
+        """type Query {
+        hello(orderBy: HelloWorldOrder! = ASC, filter: HelloWorldFilter,
+            first: Int, last: Int, before: String, after: String): [World!]!
+        }
+
+        enum HelloWorldOrder {
+        ASC
+        DESC
+        }
+
+        input HelloWorldFilter {
+        searchMassimo: String!
+        }
+
+        type World {
+        message: String!
+        }
+    """
+    )
+    config = GeneratorConfig()
+    generated_ast = generate_ast(
+        config,
+        dsl=schema,
+        stylers=[SnakeNodeName()],
+        plugins=[EnumsPlugin(), InputsPlugin()],
+    )
+
+    md = ast.Module(body=generated_ast, type_ignores=[])
+    generated = unparse(ast.fix_missing_locations(md))
+    assert "from enum import Enum" in generated, "EnumPlugin not working"
+    assert "search_massimo: str" in generated, "Snake Casing not Working"
+    assert (
+        """search_massimo: str = Field(alias='searchMassimo')""" in generated
+    ), "Automated Field aliasing not working"
