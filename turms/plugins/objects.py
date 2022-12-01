055accc051e901b51388e85e7cdea87d5e605a43
jhnnsrs <jhnnsrs@gmail.com>
2022-12-01 14:57:32 +0100

    added documentation

--- turms/plugins/objects.py
+++ turms/plugins/objects.py
@@ -390,6 +390,17 @@ def generate_types(
 
 
 class ObjectsPlugin(Plugin):
+    """Generate Pydantic models for GraphQL objects
+
+    This plugin generates Pydantic models for GraphQL ObjectTypes and Interfaces in your graphql schema.
+
+    Attention: This plugin is not made for client side usage. Most likely you are looking for the
+    operations, fragments and funcs plugin (that generates client side validation and query function
+    for your documents (your operations, fragments and queries).
+
+
+    """
+
     config: ObjectsPluginConfig = Field(default_factory=ObjectsPluginConfig)
 
     def generate_ast(
