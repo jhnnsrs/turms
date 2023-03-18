9c13795eb8703787d326c8cb69718ddc07f640b9
jhnnsrs <jhnnsrs@gmail.com>
2023-03-18 15:09:15 +0100

    more tests

--- tests/test_introspect.py
+++ tests/test_introspect.py
@@ -1,7 +1,14 @@
 import pytest
 from turms.errors import GenerationError
 from turms.helpers import load_introspection_from_url
+from turms.run import build_schema_from_schema_type, SchemaType
 from graphql import build_client_schema
+from .utils import build_relative_glob
+from pydantic import BaseModel
+
+
+class Schema(BaseModel):
+    schema_url: SchemaType
 
 
 def test_introspect():
@@ -15,3 +22,51 @@ def test_introspect_wrong():
         return build_client_schema(
             load_introspection_from_url("https://countries.sddfdf.com/")
         )
+
+
+def test_do_not_allow_schema_introspection():
+
+    s = Schema(schema_url="https://countries.trevorblades.com/")
+
+    with pytest.raises(GenerationError):
+        return build_schema_from_schema_type(s.schema_url, allow_introspection=False)
+
+
+def test_with_schema_intrsopection():
+
+    s = Schema(schema_url="https://countries.trevorblades.com/")
+
+    build_schema_from_schema_type(s.schema_url, allow_introspection=True)
+
+
+def test_build_multiple_files():
+    t = build_relative_glob("/schemas/multi_schema/*.graphql")
+
+    s = Schema(schema_url=t)
+
+    build_schema_from_schema_type(s.schema_url, allow_introspection=True)
+
+
+def test_build_multiple_files():
+    t = [
+        build_relative_glob("/schemas/multi_schema/beast_duo.graphql"),
+        build_relative_glob("/schemas/multi_schema/beast_uno.graphql"),
+    ]
+
+    s = Schema(schema_url=t)
+
+    build_schema_from_schema_type(s.schema_url, allow_introspection=True)
+
+
+def test_build_multiple_files_error():
+    t = [
+        build_relative_glob("/schemas/multi_schema/beast_duo.graphql"),
+        build_relative_glob(
+            "/schemas/multi_schema/beast_unos.graphql"
+        ),  # wrong file name
+    ]
+
+    s = Schema(schema_url=t)
+
+    with pytest.raises(GenerationError):
+        build_schema_from_schema_type(s.schema_url, allow_introspection=True)
